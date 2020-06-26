fae23768ddffd0314387f5e5a01443fda633cb5f
aunusova <49620472+aunusova@users.noreply.github.com>
2020-06-26 05:03:55 +0500

    hui

--- src/main.py
+++ src/main.py
@@ -84,9 +84,9 @@ def heatmap_maker():
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-    img.seek(0)
-    image = Image.open(img)
-    input_tensor = transform(image)
+    #img.seek(0)
+    #image = Image.open(img)
+    input_tensor = transform(img)
     img_label = 'PNEUMONIA'
     heat_img = grad_cam(model, input_tensor, heatmap_layer, img_label)
     return send_file(heat_img)
